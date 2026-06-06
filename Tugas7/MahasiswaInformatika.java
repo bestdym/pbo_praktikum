2e4d3c80915606669d6aa31d82e941b92df65da5
bestdym <ndimndimmasn@gmail.com>
2026-06-06 18:46:53 +0700

    new tugas 7

--- Tugas7/MahasiswaInformatika.java
+++ Tugas7/MahasiswaInformatika.java
@@ -2,7 +2,8 @@ public class MahasiswaInformatika extends Mahasiswa {
     private String peminatan;
     private String bahasaPemrogramanFavorit;
 
-    public MahasiswaInformatika(String nama, String email, String nim, double ipk, String peminatan, String bahasaPemrogramanFavorit) {
+    public MahasiswaInformatika(String nama, String email, String nim, double ipk, 
+    String peminatan, String bahasaPemrogramanFavorit) {
         super(nama, email, nim, ipk);
         this.peminatan = peminatan;
         this.bahasaPemrogramanFavorit = bahasaPemrogramanFavorit;
